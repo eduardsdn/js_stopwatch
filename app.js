a70e740676265b109185db77ebf6510352d2a69b
eduardsdn <eduard.sdn@gmail.com>
2023-01-08 20:16:43 +0100

    DOM elements pulled, further logic must be implemented inside template functions

---
+++ app.js
@@ -0,0 +1,19 @@
+const timer = document.getElementsByClassName("timer")
+const startBtn = document.getElementById("startBtn")
+const stopBtn = document.getElementById("stopBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+startBtn.addEventListener('click', startTimer)
+
+
+function startTimer(){
+   
+}
+
+function stopTimer(){
+
+}
+
+function resetTimer(){
+
+}
