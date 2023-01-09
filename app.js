78b678c9b7f06d4fb6864295dd5dcd00dcb1b772
eduardsdn <eduard.sdn@gmail.com>
2023-01-09 15:44:32 +0100

    Multiple start function trigger bug fixed. Seconds section no more displays 60 before starting a new seconds count

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const resetBtn = document.getElementById("resetBtn")
 const seconds = document.querySelector(".seconds")
 const minutes = document.querySelector(".minutes")
 
-startBtn.addEventListener('click', startTimer)
+startBtn.addEventListener('click', function(){
+    if(startIsClicked == false){
+    startIsClicked = true
+    startTimer()}}
+    )
+
 stopBtn.addEventListener('click', stopTimer)
 resetBtn.addEventListener('click', resetTimer)
+
+startIsClicked = false
+
 let totalSeconds = 0
 let totalMinutes = 0
-
-
 
 function startTimer(){
     interval = setInterval(updateTimer, 1000)
@@ -20,6 +26,7 @@ function startTimer(){
 
 function stopTimer(){
     clearInterval(interval)
+    startIsClicked = false
 }
 
 function resetTimer(){
@@ -34,11 +41,13 @@ function updateTimer(){
     seconds.textContent = totalSecondsStr
 
     if(totalSeconds % 60 == 0){
+        if(totalSeconds == 60){
+            seconds.textContent = '00'
+        }
         totalSeconds = 0
         totalMinutes++
         totalMinutesStr = totalMinutes.toString()
         totalMinutesStr = totalMinutesStr.padStart(2,'0')
         minutes.textContent = totalMinutesStr
     }
-    
 }
